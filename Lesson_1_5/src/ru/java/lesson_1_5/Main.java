9e56a385c3e9481d8d702b67cea6086afd15ba11
Abilis <abilis@mail.ru>
2016-04-08 22:17:53 +0400

    1

--- Lesson_1_5/src/ru/java/lesson_1_5/Main.java
+++ Lesson_1_5/src/ru/java/lesson_1_5/Main.java
@@ -1,14 +1,15 @@
 package ru.java.lesson_1_5;
 
+
+
 /**
  * Created by Abilis on 08.04.2016.
  */
 public class Main {
 
-    public static void main(String[] args) {
+    private Main k = new Main();
 
-
-        
+    public static void main(String... args) {
 
     }
 }
