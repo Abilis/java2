4ec51dc4bcc30ffb131a2581f7ef955e069f2fe1
Abilis <abilis@mail.ru>
2016-04-11 14:26:24 +0400

    Опять почти начало

---
+++ Lesson_2_3/src/test/resourses/test.sql
@@ -0,0 +1,4 @@
+#INSERT INTO users (name, age, mail) VALUES ('Alex', 22, 'alex@mail.ru');
+select * from users;
+#UPDATE users set name='Stive', age='55' WHERE id = 1;
+#DELETE FROM users WHERE id = 1;
