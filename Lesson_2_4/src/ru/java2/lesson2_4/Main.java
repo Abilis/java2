55e0667c33fadb9ed3ac0aceb2a9c24c332b4222
Abilis <abilis@mail.ru>
2016-04-11 20:26:31 +0400

    Теперь все будем делать через трай с ресурсами

--- Lesson_2_4/src/ru/java2/lesson2_4/Main.java
+++ Lesson_2_4/src/ru/java2/lesson2_4/Main.java
@@ -1,36 +1,16 @@
 package ru.java2.lesson2_4;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 
 /**
  * Created by Abilis on 11.04.2016.
  */
 public class Main {
 
-    private static Connection connection;
+
 
     public static void main(String[] args) {
 
-        DbHelper dbHelper = DbHelper.getDbHelper();
 
-        try {
-            connection = dbHelper.getConnection();
-        } catch (SQLException e) {
-            System.out.println("Не получилось подключиться к БД");
-        }
-
-        try {
-
-            Statement statement = connection.createStatement();
-
-            String query1 = "";
-            statement.execute(query1);
-        } catch (SQLException e) {
-            System.out.println("Не получилось выполнить запрос");
-        }
         
 
 
