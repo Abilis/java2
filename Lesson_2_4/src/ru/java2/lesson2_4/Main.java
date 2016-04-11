a659c27cc65c5e7a0e56cf5266f450cd407d35a9
Abilis <abilis@mail.ru>
2016-04-11 17:23:26 +0400

    Начато выполнение дз

--- Lesson_2_4/src/ru/java2/lesson2_4/Main.java
+++ Lesson_2_4/src/ru/java2/lesson2_4/Main.java
@@ -1,10 +1,18 @@
 package ru.java2.lesson2_4;
 
+import java.sql.Connection;
+
+
 /**
  * Created by Abilis on 11.04.2016.
  */
 public class Main {
 
-    
+    private Connection connection;
+
+    DbHelper dbHelper = DbHelper.getDbHelper();
+
+
+
 
 }
