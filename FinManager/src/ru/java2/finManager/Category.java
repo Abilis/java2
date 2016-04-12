6857ea69ecb9dcd0f415aef3c8d3c3a77cce34f2
Abilis <abilis@mail.ru>
2016-04-12 17:22:19 +0400

    Пока хватит ковырять финансового менеджера

--- FinManager/src/ru/java2/finManager/Category.java
+++ FinManager/src/ru/java2/finManager/Category.java
@@ -7,7 +7,16 @@ public class Category {
 
     private String description;
 
+    public String getDescription() {
+        return description;
+    }
+
     public Category(String description) {
         this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Категория: \"" + description + "\"";
     }
 }
--- FinManager/src/ru/java2/finManager/ConsoleHelper.java
+++ FinManager/src/ru/java2/finManager/ConsoleHelper.java
@@ -1,7 +1,6 @@
 package ru.java2.finManager;
 
 import java.io.BufferedReader;
-import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
