b2a7b50438f531bf575ff6c9fe9f25429e6b1d24
Abilis <abilis@mail.ru>
2016-04-18 16:16:07 +0400

    Формы, кнопки, поля ввода текста и пароля

--- Lesson_3_2/src/ru/java2/lesson_3_2/Main.java
+++ Lesson_3_2/src/ru/java2/lesson_3_2/Main.java
@@ -2,6 +2,7 @@ package ru.java2.lesson_3_2;
 
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Abilis on 18.04.2016.
@@ -11,24 +12,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        JFrame frameExternal = new JFrame("Внешняя форма");
+        JFrame mainWindow = new JFrame("Финансовый менеджер");
 
-        frameExternal.setSize(600, 400);
-        frameExternal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frameExternal.setLocationRelativeTo(null);
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainWindow.setSize(600, 400);
+        mainWindow.setLocationRelativeTo(null);
+
+        mainWindow.setLayout(new GridBagLayout());
+
+        JButton buttonLogin = new JButton("Залогиниться");
+        JButton buttonRegistration = new JButton("Регистрация");
+
+        JTextField textFieldLogin = new JTextField(10);
+        JPasswordField passwordField = new JPasswordField(10);
+
+        mainWindow.add(textFieldLogin);
+        mainWindow.add(buttonLogin);
+        mainWindow.add(passwordField);
+        mainWindow.add(buttonRegistration);
 
 
-        frameExternal.setVisible(true);
 
-
-        JFrame frameInternal = new JFrame("Внутренняя форма");
-        frameInternal.setSize(300, 100);
-        frameInternal.setLocationRelativeTo(frameExternal);
-
-
-        frameInternal.setVisible(true);
-
-
+        mainWindow.setVisible(true);
 
 
     }
