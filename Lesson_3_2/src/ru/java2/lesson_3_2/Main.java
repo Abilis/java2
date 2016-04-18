87fc953de3f712baef33740182160379e83ccceb
Abilis <abilis@mail.ru>
2016-04-18 16:35:53 +0400

    Label

--- Lesson_3_2/src/ru/java2/lesson_3_2/Main.java
+++ Lesson_3_2/src/ru/java2/lesson_3_2/Main.java
@@ -17,18 +17,30 @@ public class Main {
         mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainWindow.setSize(600, 400);
         mainWindow.setLocationRelativeTo(null);
-
         mainWindow.setLayout(new GridBagLayout());
+
 
         JButton buttonLogin = new JButton("Залогиниться");
         JButton buttonRegistration = new JButton("Регистрация");
 
+        JLabel labelLogin = new JLabel("Введите логин:");
+        JLabel labelPassword = new JLabel("Введите пароль:");
+        JLabel labelRegistration = new JLabel("Зарегистрировать нового пользователя:");
+
+
         JTextField textFieldLogin = new JTextField(10);
         JPasswordField passwordField = new JPasswordField(10);
 
+
+        mainWindow.add(labelLogin);
         mainWindow.add(textFieldLogin);
-        mainWindow.add(buttonLogin);
+
+        mainWindow.add(labelPassword);
         mainWindow.add(passwordField);
+
+        mainWindow.add(buttonLogin);
+
+        mainWindow.add(labelRegistration);
         mainWindow.add(buttonRegistration);
 
 
