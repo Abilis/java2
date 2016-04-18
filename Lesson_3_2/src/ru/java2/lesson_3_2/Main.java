0ccb82bdf6624edd51158853bae2f991ae35e559
Abilis <abilis@mail.ru>
2016-04-18 21:23:04 +0400

    BorderLayout

--- Lesson_3_2/src/ru/java2/lesson_3_2/Main.java
+++ Lesson_3_2/src/ru/java2/lesson_3_2/Main.java
@@ -17,18 +17,33 @@ public class Main {
         mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainWindow.setSize(600, 400);
         mainWindow.setLocationRelativeTo(null);
-        mainWindow.setLayout(new GridBagLayout());
+        mainWindow.setLayout(new BorderLayout());
 
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
 
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.ORANGE);
+        panel1.setBackground(Color.BLUE);
+        panel2.setBackground(Color.YELLOW);
+        panel3.setBackground(Color.ORANGE);
+        panel4.setBackground(Color.GREEN);
+        panel5.setBackground(Color.RED);
 
-        JTextField textFieldLogin = new JTextField(10);
+        mainWindow.add(panel1, BorderLayout.PAGE_START);
+        mainWindow.add(panel2, BorderLayout.PAGE_END);
+        mainWindow.add(panel3, BorderLayout.CENTER);
+        mainWindow.add(panel4, BorderLayout.LINE_END);
+        mainWindow.add(panel5, BorderLayout.LINE_START);
+
+        panel3.setLayout(new BorderLayout());
+
         JButton buttonLogin = new JButton("Залогиниться");
+        JButton buttonRegistration = new JButton("Зарегистрироваться");
 
-        panel.add(textFieldLogin);
-        panel.add(buttonLogin);
-        mainWindow.add(panel);
+        panel3.add(buttonLogin, BorderLayout.NORTH);
+        panel3.add(buttonRegistration, BorderLayout.SOUTH);
 
         mainWindow.setVisible(true);
 
