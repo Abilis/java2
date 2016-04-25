4649ef69958e838c1b93b8069f806b7aa5b244b4
Abilis <abilis@mail.ru>
2016-04-25 14:32:11 +0400

    Создана пара классов для начала выполнения ДЗ

---
+++ Lesson_4_1/src/ru/java2/lesson4_1/BinaryHeap.java
@@ -0,0 +1,11 @@
+package ru.java2.lesson4_1;
+
+/**
+ * Created by Abilis on 25.04.2016.
+ */
+public class BinaryHeap {
+
+
+
+
+}
---
+++ Lesson_4_1/src/ru/java2/lesson4_1/Main.java
@@ -0,0 +1,10 @@
+package ru.java2.lesson4_1;
+
+/**
+ * Created by Abilis on 25.04.2016.
+ */
+public class Main {
+
+    
+
+}
