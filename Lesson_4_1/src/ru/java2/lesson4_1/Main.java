f1d7ef1ac9188100c09d10deadab6a595cc9f682
Abilis <abilis@mail.ru>
2016-04-25 15:15:53 +0400

    Пока хз что делать

--- Lesson_4_1/src/ru/java2/lesson4_1/Main.java
+++ Lesson_4_1/src/ru/java2/lesson4_1/Main.java
@@ -1,10 +1,35 @@
 package ru.java2.lesson4_1;
 
+
+import java.util.*;
+
 /**
  * Created by Abilis on 25.04.2016.
  */
 public class Main {
 
-    
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3);
+
+        List<Integer> res = map(numbers, new
+                SquareOperator());
+    }
+
+    interface Operator<R, T> {
+        R apply(T t);
+    }
+
+    private static class SquareOperator implements Operator {
+
+        @Override
+        public Object apply(Object o) {
+            return null;
+        }
+
+        public void map() {
+            
+        }
+    }
 
 }
