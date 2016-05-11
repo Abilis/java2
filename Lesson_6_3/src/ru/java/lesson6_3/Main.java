ee2f1646805f4be7d3224311235d98b6fcf690f2
Abilis <abilis@mail.ru>
2016-05-11 12:31:48 +0400

    Начата выполнение сложной части

---
+++ Lesson_6_3/src/ru/java/lesson6_3/Main.java
@@ -0,0 +1,7 @@
+package ru.java.lesson6_3;
+
+/**
+ * Created by Abilis on 11.05.2016.
+ */
+public class Main {
+}
---
+++ Lesson_6_3/src/ru/java/lesson6_3/generateFiles/Generator.java
@@ -0,0 +1,17 @@
+package ru.java.lesson6_3.generateFiles;
+
+/**
+ * Created by Abilis on 11.05.2016.
+ */
+public class Generator {
+
+    private String fileName = "D:\\temp\\testfile\\testdir";
+    private int amountFiles = 10;
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
