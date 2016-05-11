b873325d8039abaa1886deb776cfdac4d1d6ad1a
Abilis <abilis@mail.ru>
2016-05-11 17:09:22 +0400

    Логика производителя вроде написана

--- Lesson_6_3/src/ru/java/lesson6_3/Producer.java
+++ Lesson_6_3/src/ru/java/lesson6_3/Producer.java
@@ -9,7 +9,8 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class Producer extends Thread {
 
     private static ConcurrentLinkedQueue<File> listOfFiles;
-    private static int countFiles = 1;
+    private static int countFiles = 0;
+    private static int countString = 1;
 
     public Producer(ConcurrentLinkedQueue<File> listOfFiles) {
         this.listOfFiles = listOfFiles;
@@ -18,6 +19,47 @@ public class Producer extends Thread {
     @Override
     public void run() {
 
+        while (!isInterrupted()) {
+            File currentFile = getFile();   //берем из очереди очередной файл
+            if (currentFile == null) {      //если из очереди вернулся нулл - файлы кончились
+                break;
+            }
+
+            System.out.println("Обработка файла " + ++countFiles);
+
+            BufferedReader bufferedReader = null;
+            try {
+                bufferedReader = new BufferedReader(new FileReader(currentFile)); //Открываем новый поток на чтение из файла
+                String line = null;
+
+                while ((line = bufferedReader.readLine()) != null) { //читаем построчно файл
+
+                    if (Buffer.addString(line)) {                    //суем прочитанную строку в очередь-буфер
+                        countString++;
+                    }
+                    else {
+                        System.out.println("Работа производителя прервана по таймауту!");
+                        interrupt();
+                        break;
+                    }
+
+                }
+
+
+
+            } catch (IOException e1) {
+                System.out.println("IOException: " + e1.getMessage());
+
+            } catch (InterruptedException e2) {
+                System.out.println("Работа производителя " + Thread.currentThread().getName() + " прервана!");
+            }
+
+            Util.closeQuitely(bufferedReader);
+
+        }
+
+        System.out.println("Работа производителя " + Thread.currentThread().getName() + " завершена!" +
+                " Обработано строк: " + countString + ", обработано файлов: " + countFiles);
 
 
     }
